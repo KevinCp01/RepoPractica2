1b29ede528c30a7d940e3446812b4d4ebe53cc40
Kevin Campa <kevincampa01@gmail.com>
2022-03-23 20:55:01 -0700

    Se modifico el archivo Prueba.java

--- Prueba.java
+++ Prueba.java
@@ -1,6 +1,8 @@
 class prueba{
     
     public static void main (String[] args){
-	System.out.println("Esta es la practica 2 de jenkings, saludos a su abue");
+	
+	    System.out.println("Esta es la practica 2 de jenkings, saludos a su abue");
+	    System.out.println("Conexion con le webhook");
     }
 }
