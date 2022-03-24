80c0495ff775c90b121685c819fd81a30082ab07
Kevin Campa <kevincampa01@gmail.com>
2022-03-23 21:13:07 -0700

    Se modifico archivo prueba de nuevo

--- Prueba.java
+++ Prueba.java
@@ -3,6 +3,6 @@ class prueba{
     public static void main (String[] args){
 	
 	    System.out.println("Esta es la practica 2 de jenkings, saludos a su abue");
-	    System.out.println("Conexion con le webhook");
+	    System.out.println("Conexion con el web-hook confirmada");
     }
 }
